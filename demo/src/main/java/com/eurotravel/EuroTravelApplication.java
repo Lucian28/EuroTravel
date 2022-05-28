c75c1279e57319cdb98c2d391568d8fc8e8a5066
Lucian28 <lucian.miholca@gmail.com>
2022-05-28 15:53:52 +0300

    CRED-0 - add main class to git and settings.gradle

---
+++ demo/src/main/java/com/eurotravel/EuroTravelApplication.java
@@ -0,0 +1,13 @@
+package com.eurotravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroTravelApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/eurotravel/EuroTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurotravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
